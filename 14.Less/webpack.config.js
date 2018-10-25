48c7bec8d45c25c91eda3fe7cfba33574cc91862
Rodrigo (Roy) <chavo0022009@gmail.com>
2018-10-24 21:53:52 -0500

    14. Se agregó ejemplo de configuración para LESS

---
+++ 14.Less/webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/js/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: './dist/',
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/styles.css",
+            //filename: "css/[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    module: {
+        rules: [{
+                //Loaders Zone
+
+                test: /\.css$/ /*file type*/ ,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
+                    }
+                }, 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'less-loader',
+                    options: {
+                        noIeCompat: true
+                    }
+                }]
+            },
+            {
+                //Loaders Zone
+
+                test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/ /*file type*/ ,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1000000,
+                    }
+                }
+            },
+            {
+                //Loaders Zone
+
+                test: /\.(mp4)$/ /*file type*/ ,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1000000,
+                        name: 'videos/[name].[hash].[ext]'
+                    }
+                }
+            },
+            {
+                //Loaders Zone
+
+                test: /\.js$/ /*file type*/ ,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
