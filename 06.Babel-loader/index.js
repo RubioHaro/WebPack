f3d11dfcbf18c2a2f20cc4c27da6c4a183ed1d11
Rodrigo (Roy) <chavo0022009@gmail.com>
2018-10-24 21:45:14 -0500

    06. Se agregó el loader de Babel

---
+++ 06.Babel-loader/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+import {messages} from './message.js';
+
+document.write(messages.firstMessage);
+messages.errorMessage();
+
+console.log('hola mundo');
---
+++ 06.Babel-loader/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/styles.css",
+            //filename: "css/[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                //Loaders Zone
+
+                test: /\.css$/ /*file type*/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
+                    }
+                }, 'css-loader'
+                ]
+            },
+            {
+                //Loaders Zone
+
+                test: /\.js$/ /*file type*/,
+                use: {loader: 'babel-loader'}
+            }
+        ]
+    }
+}
+
+module.exports = config
